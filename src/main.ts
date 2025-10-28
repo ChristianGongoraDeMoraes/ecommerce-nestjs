286ddcded84183e3ecb4583fca1e1bd7b112afab
chris <christianpimpo304@hotmail.com>
2025-10-28 06:33:53 -0300

    feat: validation global pipe, http-> 400

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Ativa o ValidationPipe globalmente
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove propriedades que não estão no DTO
+      forbidNonWhitelisted: true, // lança erro se vier campos extras
+      transform: true, // converte tipos automaticamente (ex: string -> number)
+      exceptionFactory: (errors) => {
+        // Personaliza o formato do erro
+        return {
+          statusCode: 400,
+          message: 'Erro de validação nos dados enviados',
+          errors: errors.map((err) => ({
+            campo: err.property,
+            erros: Object.values(err.constraints || {}),
+          })),
+        };
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
